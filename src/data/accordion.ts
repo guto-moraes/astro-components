9706edfeda001e1dfa2c988bed48330e28d1b23b
FSM <joseaugsto.teo@gmail.com>
2024-02-02 01:59:17 -0300

    Criação do Astro Accordion Component acessível

---
+++ src/data/accordion.ts
@@ -0,0 +1,22 @@
+export const accordions = [
+  {
+    title: "First Accordion Title",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos laboriosam molestiae sit ipsam at nostrum sed quam, delectus accusantium, consequatur minus? Rerum delectus obcaecati impedit. Minima, animi. Sed placeat eum quaerat exercitationem, saepe accusamus enim dolor praesentium ex earum voluptas ea ratione veritatis. Sed facere inventore voluptates, provident dolore vero?",
+  },
+  {
+    title: "Second Accordion Title",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos laboriosam molestiae sit ipsam at nostrum sed quam, delectus accusantium, consequatur minus? Rerum delectus obcaecati impedit. Minima, animi. Sed placeat eum quaerat exercitationem, saepe accusamus enim dolor praesentium ex earum voluptas ea ratione veritatis. Sed facere inventore voluptates, provident dolore vero?",
+  },
+  {
+    title: "Third Accordion Title",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos laboriosam molestiae sit ipsam at nostrum sed quam, delectus accusantium, consequatur minus? Rerum delectus obcaecati impedit. Minima, animi. Sed placeat eum quaerat exercitationem, saepe accusamus enim dolor praesentium ex earum voluptas ea ratione veritatis. Sed facere inventore voluptates, provident dolore vero?",
+  },
+  {
+    title: "Fourth Accordion Title",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos laboriosam molestiae sit ipsam at nostrum sed quam, delectus accusantium, consequatur minus? Rerum delectus obcaecati impedit. Minima, animi. Sed placeat eum quaerat exercitationem, saepe accusamus enim dolor praesentium ex earum voluptas ea ratione veritatis. Sed facere inventore voluptates, provident dolore vero?",
+  },
+];
